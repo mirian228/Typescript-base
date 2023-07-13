1ff768283991b3fcd8f37a7c829cd6d5a583599e
mirian228 <pariksini@mail.ru>
2023-07-13 17:17:50 +0400

    Generics

---
+++ dist/src/generics.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const score = [];
+const names = [];
+function identityOne(val) {
+    return val;
+}
---
+++ src/generics.ts
@@ -0,0 +1,9 @@
+const score: Array<number> = []
+const names: Array<string> = []
+
+function identityOne(val: boolean | number): boolean | number {
+    return val;
+}
+
+
+export {}
