331ca39f97369c235aa6cc971ac162b7c60f6954
mirian228 <pariksini@mail.ru>
2023-07-13 16:34:43 +0400

    classes

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log("Typescript is here");
+Object.defineProperty(exports, "__esModule", { value: true });
+class User {
+    constructor(id, email, name, city) {
+        this.id = id,
+            this.email = email,
+            this.name = name,
+            this.city = city;
+    }
+    drive() {
+        return "driving...";
+    }
+    get getId() {
+        return this.id;
+    }
+    get getEmail() {
+        return this.email;
+    }
+    set setEmail(email) {
+        this.email = email;
+    }
+}
+const Mirian = new User(1, "m@mail.com", "mirian", "batumi");
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log("Typescript is here");
+class User {
+    private readonly id: number
+    private email: string
+    private name: string
+    private city: string
+
+    constructor(id:number, email: string, name: string, city: string) {
+        this.id = id,
+        this.email = email,
+        this.name = name,
+        this.city = city
+    }
+
+   
+    drive(): string {
+        return "driving..."
+    }
+
+    get getId(): number {
+        return this.id;
+    }
+
+    get getEmail(): string {
+        return this.email;
+    }
+
+    set setEmail(email: string) {
+        this.email = email;
+    } 
+
+}
+
+
+const Mirian = new User(1, "m@mail.com", "mirian", "batumi")
+
+
+export {}
